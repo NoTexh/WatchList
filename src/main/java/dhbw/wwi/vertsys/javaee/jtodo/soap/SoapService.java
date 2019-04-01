243fc7479315319130e98604fdcbadadfb9dcaa5
Kai Schmid <kai.schmid70@gmail.com>
2019-04-01 18:25:12 +0200

    Soap klasse erstellt, anfangsmethode erstellt

---
+++ src/main/java/dhbw/wwi/vertsys/javaee/jtodo/soap/SoapService.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2019 Markus Scheuring, Kai Schmid, Tobias Frietsch
+ * 
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ * 
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbw.wwi.vertsys.javaee.jtodo.soap;
+
+import dhbwka.wwi.vertsys.javaee.jtodo.common.ejb.UserBean;
+import dhbwka.wwi.vertsys.javaee.jtodo.tasks.ejb.MovieBean;
+import dhbwka.wwi.vertsys.javaee.jtodo.tasks.jpa.Movie;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+@WebService
+@Stateless
+public class SoapService {
+    
+   @EJB MovieBean movieBean;
+   @EJB UserBean userBean;
+ 
+   @WebMethod
+   @WebResult(name = "movies")
+   public List<Movie> findMovies(
+           @WebParam(name="username") String username,
+           @WebParam(name="password") String password
+   ) throws UserBean.InvalidCredentialsException
+   {     
+       this.userBean.validateUser(username, password);
+       return movieBean.findAll();
+   }
+   
+}
